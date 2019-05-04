c036bc6b77d1d4644640c4061e1d3ff3c60beed9
wnqui <49946406+wnqui@users.noreply.github.com>
2019-05-05 04:15:06 +0800

    beta version

--- src/Ball.java
+++ src/Ball.java
@@ -1,34 +1,180 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 public class Ball {
 
 	private int x;
 	private int y;
+
 	protected int size;
-	
-	protected int dx;
-	protected int dy;
-	
-	public Ball(int x ,int y, int size) {
+	protected double r;
+
+	protected double dx;
+	protected double dy;
+	public static int power = 10;
+
+	protected Color color;
+
+	public Ball(int x, int y, int size) {
+		
 		this.setX(x);
 		this.setY(y);
 		this.size = size;
+		this.r = size / 2;
+
+		int r1 = (int) (Math.random() * power + 1);
+
+		dx = r1;
+		dy = power - r1;
+
+		int r2 = (int) (Math.random() * 3 + 1);
+
+		switch (r2) {
+		case 1:
+			dx *= -1;
+			break;
+		case 2:
+			dy *= -1;
+			break;
+		case 3:
+			dx *= -1;
+			dy *= -1;
+			break;
+		}
+
+		int r3 = (int) (Math.random() * 5 + 1);
+		switch (r3) {
+		case 1:
+			color = Color.RED;
+			break;
+		case 2:
+			color = Color.BLUE;
+			break;
+		case 3:
+			color = Color.YELLOW;
+			break;
+		case 4:
+			color = Color.GREEN;
+			break;
+		case 5:
+			color = Color.MAGENTA;
+			break;
+		}
+
 	}
 
-	public void run() {
-		
+	public void run(ArrayList<Ball> balls) {
+
+		// 邊界反彈
+		outsideCheck();
+		// 碰撞反彈
+		isCollision(balls);
+		// 移動
+		this.x += dx;
+		this.y += dy;
+		// 移動速度補正
+
 	}
-	
+	//邊境檢查
+	public void outsideCheck() {
+
+		if (this.y <= CollisionBall.UP_Y) {
+			this.dy *= -1;
+			this.y = CollisionBall.UP_Y + 1;
+
+		} else if (this.y + size >= CollisionBall.DN_Y) {
+			this.dy *= -1;
+			this.y = CollisionBall.DN_Y - size - 1;
+		}
+		if (this.x <= CollisionBall.L_X) {
+			this.dx *= -1;
+			this.x = CollisionBall.L_X + 1;
+		} else if (this.x + size >= CollisionBall.R_X) {
+			this.dx *= -1;
+			this.x = CollisionBall.R_X - size - 1;
+		}
+
+	}
+
+	// collision check
+	public void isCollision(ArrayList<Ball> balls) {
+		double dr = 0;
+		double angle1 = 0;
+		double angle2 = 0;
+		Ball b1;
+		Ball b2;
+		for (int i = balls.size(); i > 0; i--) {
+
+			for (int j = 1; j < i; j++) {
+				b1 = balls.get(i - 1);
+				b2 = balls.get(j - 1);
+				dr = Math.sqrt(Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2));
+				if (dr <= (b1.r + b2.r)) {
+					angle1 = Math.atan2(b1.x - b2.x, b1.y - b2.y) / Math.PI * 180;
+					angle2 = Math.atan2(b2.x - b1.x, b2.y - b1.y) / Math.PI * 180;
+
+					balls.get(i - 1).dx = (int) (power * Math.cos(Math.toRadians(angle1)));
+					balls.get(i - 1).dy = (int) (power * Math.sin(Math.toRadians(angle1)));
+
+					balls.get(j - 1).dx = (int) (power * Math.cos(Math.toRadians(angle2)));
+					balls.get(j - 1).dy = (int) (power * Math.sin(Math.toRadians(angle2)));
+
+				}
+			}
+		}
+
+	}
+	//重設動量
+	public void reset() {
+		int r1 = (int) (Math.random() * power + 1);
+
+		dx = r1;
+		dy = power - r1;
+
+		int r2 = (int) (Math.random() * 3 + 1);
+
+		switch (r2) {
+		case 1:
+			dx *= -1;
+			x += dx * 10;
+			break;
+		case 2:
+			dy *= -1;
+			y += dy * 10;
+			break;
+		case 3:
+			dx *= -1;
+			dy *= -1;
+			x += dx * 10;
+			y += dy * 10;
+			break;
+
+		}
+	}
+
+	// drawball
+	public void drawBall(Graphics2D g2) {
+		g2.setColor(color);
+		g2.fillOval(x, y, size, size);
+		g2.setColor(Color.black);
+		g2.drawOval(x, y, size, size);
+	}
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 }
--- src/CollisionBall.java
+++ src/CollisionBall.java
@@ -1,9 +1,12 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -14,16 +17,21 @@ import javax.swing.Timer;
  * Random collision practice of the ball
  */
 
-public class CollisionBall implements ActionListener {
-	//variable
-	public static final int WIDTH =800;
+public class CollisionBall implements ActionListener, KeyListener {
+	// variable
+	public static final int WIDTH = 800;
 	public static final int HEIGHT = 800;
-	//object
+	public static final int UP_Y = 70;
+	public static final int DN_Y = HEIGHT - 170 + 70;
+	public static final int L_X = 50;
+	public static final int R_X = WIDTH - 2 * L_X + 50;
+	// object
 	public static JFrame JF;
-	public static myJpanel	MP;
+	public static myJpanel MP;
 	public static CollisionBall CB;
-	public ArrayList<Ball>ball = new ArrayList<Ball>();
-	//control
+	public static ArrayList<Ball> balls = new ArrayList<Ball>();
+
+	// control
 
 	public CollisionBall() {
 		JF = new JFrame("Collision Ball");
@@ -34,42 +42,94 @@ public class CollisionBall implements ActionListener {
 		JF.setAlwaysOnTop(true);
 		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JF.add(MP);
-		
-		
-		Timer T = new Timer(20,this);
+		JF.addKeyListener(this);
+
+		Timer T = new Timer(20, this);
 		T.start();
-		
+
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
-		
-		
+
+		for (int i = 0; i < balls.size(); i++) {
+
+			balls.get(i).run(balls);
+
+		}
+
 		MP.repaint();
-		
+
 	}
-	//draw 
+
+	// draw
 	public void repaint(Graphics g) {
-		Graphics2D g2 = (Graphics2D)g;
+		Graphics2D g2 = (Graphics2D) g;
 		g2.setStroke(new BasicStroke(3));
-		//白底
+		// 白底
 		g2.setColor(Color.white);
 		g2.fillRect(0, 0, WIDTH, HEIGHT);
+		// 邊線
+		g2.setColor(Color.black);
+		g2.drawRect(L_X, UP_Y, R_X - 50, DN_Y - 70);
+		// 提示文字
+		g2.setColor(Color.GRAY);
+		g2.setFont(new Font("", 1, 20));
+		g2.drawString("type A add one           type D delete one           type R random all", 50, 65);
+
+		for (int i = 0; i < balls.size(); i++) {
+			balls.get(i).drawBall(g2);
+		}
+
 	}
-	
 
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			int x = (int) (Math.random() * 500 + 100);
+			int y = (int) (Math.random() * 500 + 100);
+			int s = (int) (Math.random() * 30 + 30);
+			balls.add(new Ball(x, y, s));
+		}
 		CB = new CollisionBall();
 	}
-	
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			int x = (int) (Math.random() * 500 + 100);
+			int y = (int) (Math.random() * 500 + 100);
+			int s = (int) (Math.random() * 30 + 30);
+			balls.add(new Ball(x, y, s));
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			if (balls.size() >= 1) {
+				balls.remove(0);
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_R) {
+			for (int i = 0; i < balls.size(); i++) {
+				balls.get(i).reset();
+			}
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
 //end
 }
 
-
-class myJpanel extends JPanel{
+class myJpanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+
 	public void paintComponent(Graphics g) {
 		CollisionBall.CB.repaint(g);
 	}
-	
+
 }
